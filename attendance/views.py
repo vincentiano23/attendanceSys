b78b764ff316b5d79e0e59d896ca748142d5fd3c
vincentiano23 <vincentmuthiani15@gmail.com>
2024-09-15 19:02:37 +0300

    fix attendance statistics graph rendering and update functionality

--- attendance/views.py
+++ attendance/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Student, Attendance
-import matplotlib.pyplot as plt
-import pandas as pd
-from io import BytesIO
-import base64
 from django.utils import timezone
 from django.contrib import messages
-import matplotlib
-matplotlib.use('Agg')
-
 
 
 def mark_attendance(request):
@@ -43,28 +36,6 @@ def attendance_statistics(request):
         attendance_data.append({'name': student.name, 'percentage': percentage})
 
   
-    graph = generate_bar_graph(attendance_data)
 
     return render(request, 'attendance/attendance_statistics.html', {'attendance_data': attendance_data})
 
-def generate_bar_graph(attendance_data):
-    students = [student['name'] for student in attendance_data]
-    attendance_percentages = [student['percentage'] for student in attendance_data]
-
-    plt.figure(figsize=(2,1))
-    plt.bar(students, attendance_percentages, color='blue')
-    plt.xlabel('Students')
-    plt.ylabel('Attendance Percentage')
-    plt.title('Student Attendance Statistics')
-    plt.xticks(rotation=45, ha='right')
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png',bbox_inches='tight')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    graph = base64.b64encode(image_png).decode('utf-8')
-    buffer.close()
-    plt.close()
-
- 
-    return graph
